afa2d46abd1e45490d3b24d9006ed51433aa1626
sulivan7 <devsulivan@gmail.com>
2025-09-14 23:12:24 -0300

    criando teste para inserção de comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente Post", () => {
+    it("Deve renderizar o componente corretamente", () => {
+        render(<PostComment />);
+        expect(screen.getByText("Comentar")).toBeInTheDocument();
     });
-});
+
+    test("Deve adicionar 2 comentarios", () => {
+        render(<PostComment />);
+        fireEvent.change(screen.getByTestId("campo-comentario"), {
+            target: { 
+                value: "Primeiro comentário" 
+            },
+        });
+        fireEvent.click(screen.getByTestId("btn-comentar"));
+        expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
+
+        fireEvent.change(screen.getByTestId("campo-comentario"), {
+            target: { 
+                value: "Segundo comentário" 
+            },
+        });
+        fireEvent.click(screen.getByTestId("btn-comentar"));
+        expect(screen.getByText("Segundo comentário")).toBeInTheDocument();
+        });
+    });
